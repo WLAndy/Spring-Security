3f9b649221c04e6b67fe75427ba4f3fcb4cf22f8
Andy <952928837@qq.com>
2017-11-16 22:47:05 +0800

    1

--- src/main/java/com/company/project/security/WebSecurityConfig.java
+++ src/main/java/com/company/project/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.company.project.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -14,6 +15,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Value("${spring.security.anyRequest.url}")
+    private String anyRequestUrl;
+
     @Bean
     CustomUserService customUserService() {
         return new CustomUserService();
@@ -27,8 +31,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers(anyRequestUrl.split(",")).permitAll()
                 .anyRequest().authenticated()
-                .and().formLogin().loginPage("/login").failureUrl("/login?error").permitAll().and()
-                .logout().permitAll();
+                .antMatchers("/hello").permitAll().anyRequest().hasAnyRole("ROLE_ADMIN")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
     }
 }
